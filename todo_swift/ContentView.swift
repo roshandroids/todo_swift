34396b22de98b692b824ede5002c232550a88ff8
Roshan Shrestha <shrestharoshan776@gmail.com>
2022-11-08 20:32:19 +0545

    Initial Commit

---
+++ todo_swift/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  todo_swift
+//
+//  Created by Roshan Shrestha on 08/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ todo_swift/todo_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_swiftApp.swift
+//  todo_swift
+//
+//  Created by Roshan Shrestha on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct todo_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
